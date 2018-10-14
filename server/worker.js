81799fe2af3589951720b58a88cd339ab4067e51
ehiggins98 <ericdhiggins@gmail.com>
2018-10-13 21:20:27 -0500

    Added endpoint to check status

--- server/worker.js
+++ server/worker.js
@@ -11,13 +11,20 @@ var app = express();
 app.use(express.json());
 
 var HTTP_PORT = 80;
+var executing = false;
 
 http.createServer(app).listen(HTTP_PORT,function() {
   console.log('Listening HTTP on port ' + HTTP_PORT);
 });
 
+app.get('/', function(req, res) {
+  executing ? res.sendStatus(404) : res.sendStatus(200);
+});
+
 app.post('/', function(req, res) {
+  executing = true;
   let params = req.body["params"];
   let code = req.body["code"];
   res.send(executor(params, code));
+  executing = false;
 });

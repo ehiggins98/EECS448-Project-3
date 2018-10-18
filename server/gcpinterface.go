ec6fb627156722dfdf300f40b4cf63a9456d3abf
ehiggins98 <ericdhiggins@gmail.com>
2018-10-17 21:21:40 -0500

    Fixed private key in file and started adding documentation

--- server/gcpinterface.go
+++ server/gcpinterface.go
@@ -6,16 +6,31 @@ import (
   "google.golang.org/api/compute/v1"
   "time"
   "fmt"
+  "encoding/json"
+  "os"
+  "io/ioutil"
 )
 
+type Data struct {
+  ClientEmail string `json:"client_email"`
+  PrivateKey string `json:"private_key"`
+}
+
+///The Google Compute Engine interface
 var service *compute.Service
+
+///The project name in Google Cloud
 const project = "eecs448-project-3"
+
+///The zone in Google Cloud
 const zone = "us-central1-c"
 
+///Authenticate with Google Cloud using JWT and the existing service account.
 func Init() {
+  data := getAuthInfo()
   conf := &jwt.Config {
-    Email: "router@eecs448-project-3.iam.gserviceaccount.com",
-    PrivateKey: []byte("-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC2ns6EjA4VNIQW\nBcOFq8W8PDCGMD/fJs7Jvtwrw+38i6vCoadmIr8zc1xVf0Dw+eXl27z2VT2Dt/K9\nQy1XLm754EM+wwqibnA/CEDtjIchu4jPDLsOX1J0x+cmzKiuTDICzVdLtcZNlftb\nifIG1tntsDZriAlKi3UdhI4Nmo3xjM0UFr27HpSyWxRV2EH73mYVmaEqAF3y8z8r\niPgEU/ww5Xyz+j0ooex4kqmxe3CTE9lcpQVCW/kMO/fJKhG/4OlVLXoY5TxmJkg2\ndC7RzOXUNGOwlts1plYckqaQ9u2yiX1gM/ICDa6Gsu30FbC6YNKQUuJmXS/fWNZ1\nEWCxrKqTAgMBAAECggEADb6ZE3/JBY6MzG2FoDdj6/1pt4fFHuH4WVXMk1ytOj82\n37Cz/StrUY+CqQqpVBmy1GB78dxLg3DiS51VOcUMZDEdTol2cPA38X6JCHf1qbPI\nIbE3GOdSuOhcvN5VabzhXLWgttmJAcjigKq6tQoRn+KNzf0QmfQyDdUurdZujXnm\n7RiLBRo6i08y3zuCSmzpYclZqDR7jxL6MGwMWvNOgkvCjgCUK/liijNjQpW77Yvx\nd9wY2hIq9NWc9Hs0L6mdWGAfu3ZNVdxAQhdph+SdMzeUG2Dgx6idyfDYJgHJuFdz\nLSdEGHWMhEq9Z2diePdid7h/0fWp86UxK5Z/QrlNwQKBgQD/IFZa/+s2LQZ5cvNI\nhZwGNiRHwkrwtDoUkSMQq7d46m5zUJOrOUtDNWQtNJKjk1wDkH78qptaF8P965va\nTgCAyJG2SZkLKQzucox8mf/DvmHPNuNEe2aHaY0RKkZWpGruEJQ8el29AeW+Z6Ki\nlbswMr3X/jFHVmw8dncD2UoJwQKBgQC3Pue/DhK3UiTSMKXWCwxHLw/cRQ7JNcTA\npzSIImsu7m1xW46GR8zdPIrEhwfCRE+/YvfvTvXyinU8eZzRY50/oEfItbwZxWlg\nFFD4ZdRk98891FjHxZGoUNhFCmnqX/vdAD6q+4WlSPJcxcjhIIc/9NkvIPbyxOn2\n1I4Ep5HBUwKBgAY8gh/jVZqTay8Y0j2ZloDIXgarBy3vGeRaz+Keb/Oyt1R2ScXL\ntr1D1tkMCfGZrowfwhrnCLkhD5drZPnnIjgDrxwnnGgbbsd9YVXCZfAg/T8VdmS8\nJ3tz5xDeWa3QgxSirxzzWMs/+p+25NDYnCTHeMCI5Cd8Q1UPCEW90AOBAoGASxXl\nk+3KUX/BQrdYXJpuT4TDNPi/FEeJ9X8OEXI5BSQBiF+ByRgGo+i428qQrVOcccm1\n2kM6mEWPwFX8off1aSrd/yooh07S3OG2Q/JF05GPQ8CNGF6mTpfB5phbygPGikod\nY6Zons+DL+yDYWwYv2Yu0Bbr2ZJCZDe4ccPP/60CgYAa+FTYWfqYBLnRX3tI+vv3\nmnyPey/IzuMosg6kTKuDcMAFuq/wCuqBMaYIL3+H7hA3OzK9t/1reL7pxoROodMp\nW/dIFP+AWS0yzcJBTN++FLNkZjmw8Cq3mJC52lxZ21+GgyKf/5QUxVnyQEvGkK8I\nNgdQl0/g1jLmD5N9fhRPEw==\n-----END PRIVATE KEY-----\n"),
+    Email: data.ClientEmail,
+    PrivateKey: []byte(data.PrivateKey),
     Scopes: []string{
       "https://www.googleapis.com/auth/compute",
     },
@@ -25,15 +40,34 @@ func Init() {
   service, _ = compute.New(conf.Client(oauth2.NoContext))
 }
 
+///Gets the authentication information for Google Cloud from the proper file.
+func getAuthInfo() Data {
+  jsonFile, err := os.Open("key.json")
+
+  if err != nil {
+    fmt.Println(err)
+    return Data{}
+  }
+
+  defer jsonFile.Close()
+
+  bytes, _ := ioutil.ReadAll(jsonFile)
+  var data Data
+  json.Unmarshal(bytes, &data)
+  return data
+}
+
+///Get the internal IP address of a given worker instance.
 func GetIP(id string) (string, error) {
   res, err := service.Instances.Get(project, zone, id).Do()
   if err == nil {
-    return res.NetworkInterfaces[0].NetworkIP, nil;
+    return res.NetworkInterfaces[0].NetworkIP, nil
   } else {
-    return "", err;
+    return "", err
   }
 }
 
+///Wait until the given instance is in the expected state. This will timeout after 60 seconds.
 func wait(id string, expected string) {
   status := ""
   start := TimeMillis()
@@ -43,6 +77,7 @@ func wait(id string, expected string) {
   }
 }
 
+///Reset a worker instance. This resets the VM to its initial state, stops the instance, and restarts it.
 func Reset(id string) error {
   fmt.Println("Resetting instance: " + id)
   _, err := service.Instances.Reset(project, zone, id).Do()
--- server/router.go
+++ server/router.go
@@ -11,10 +11,12 @@ import (
 var workers map[string]int64
 var postClient http.Client
 
+///Gets the current system time in milliseconds.
 func TimeMillis() int64 {
   return time.Now().UnixNano() / int64(time.Millisecond);
 }
 
+///Gets the IP address of an available execution server from workers.
 func getExecutor() (string, error) {
   for k := range workers {
     delete(workers, k)
@@ -23,6 +25,10 @@ func getExecutor() (string, error) {
   return "", errors.New("Length of workers is 0")
 }
 
+///Builds a request with properties
+/// URL: http://{server}
+/// Method: POST
+/// Headers: {Content-Type: application/json}
 func makeRequest(server string, body string) *http.Request {
   url := "http://" + server
   req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
@@ -30,6 +36,8 @@ func makeRequest(server string, body string) *http.Request {
   return req;
 }
 
+///Accepts a request from the user and forwards it to the appropriate worker.
+///If the server doesn't respond, it will be reset and rebooted asychronously.
 func forward(w http.ResponseWriter, r *http.Request) {
   body, _ := ioutil.ReadAll(r.Body)
 
@@ -55,6 +63,7 @@ func forward(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+///Main function. Initializes the server and the list of available workers.
 func main() {
   workers = map[string]int64{"worker-1": TimeMillis(), "worker-2": TimeMillis()}
   timeout := time.Duration(8 * time.Second)

b842251c4c638e11a1efa7a07fc471570f869806
ehiggins98 <ericdhiggins@gmail.com>
2018-11-18 14:45:35 -0600

    Added conditionals, loops, and function calls

---
+++ character_classifier/context_test_manual.py
@@ -0,0 +1,12 @@
+import context
+
+top_level = context.Scope([])
+
+while True:
+    print(top_level.get_valid_characters())
+    char = input()
+    if char == '_':
+        break
+    top_level.put_character(char)
+
+print(top_level.to_string())
